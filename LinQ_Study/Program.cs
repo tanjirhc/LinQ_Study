ef2fe7e681b368ea51c1118959d4edb0c356685b
tanjirhc <130759310+tanjirhc@users.noreply.github.com>
2024-03-06 22:53:25 +0600

    Add project files.

---
+++ LinQ_Study/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinQ_Study
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> Name = new List<string> { "Ehsanul", "Tamim", "Nafis"};
+            foreach (string s in Name) 
+            { 
+                Console.WriteLine(s);
+            }
+            Console.ReadLine();
+        }
+    }
+}
