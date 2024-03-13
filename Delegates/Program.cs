3f7242c742904c702f2622f657163cadddc3d21e
tanjirhc <130759310+tanjirhc@users.noreply.github.com>
2024-03-13 23:38:38 +0600

    Delegates Learning

---
+++ Delegates/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    public delegate void PrinteDelegate(string message);
+    public class Program
+    {
+        private static void Print(string message)
+        {
+            Console.WriteLine(message);
+        }
+        private static void Print2(string msg) 
+        { 
+            Console.WriteLine(msg+"Print int from method 2");
+        }
+        public static void calculate(int a, int b, PrinteDelegate printer)
+        {
+            int result = a + b;
+            printer(result.ToString());
+        }
+        static void Main(string[] args)
+        {
+            calculate(1, 2, Print2);
+            Console.ReadLine();
+        }
+    }
+}
